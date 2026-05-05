65802b28030acc72e2ea78ff5763e48743378ab5
TransmetalAG <transmetalag@gmail.com>
2026-05-05 09:17:38 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import AuditoriaACII from "./components/AuditoriaACII";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Auditoría ACII</h1>
+      <AuditoriaACII />
+    </div>
+  );
+}
